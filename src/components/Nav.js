50df8ff4a9730fba3d40b65d445776aeb0c78bb4
8ark-dev <sorlti6952@gmail.com>
2023-02-10 16:19:02 +0900

    REFACTOR , DESIGN nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,68 +3,67 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 export default function Nav() {
-  const [show, setShow] = useState(false);
-  const [searchValue, setSearchValue] = useState("");
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   window.addEventListener("scroll", () => {
-  //     console.log("window.scrollY", window.scrollY);
-  //     if (window.scrollY > 30) {
-  //       setShow(true);
-  //     } else {
-  //       setShow(false);
-  //     }
-  //   });
-
-  //   return () => {
-  //     window.removeEventListener("scroll", () => {});
-  //   };
-  // }, []);
-
-  const handleChange = (e) => {
-    setSearchValue(e.target.value);
-    navigate(`/?user=${e.target.value}`);
-  };
-
   return (
-    <Container className={`nav`}>
-      <img
-        alt="Helloa logo"
-        src="http://via.placeholder.com/80x40"
-        className="nav__logo"
-        onClick={() => (window.location.href = "/")}
-      />
+    <Container>
+        <ul>
+          <MenuList>LeaderBoard</MenuList>
+          <MenuList>Menu1</MenuList>
+          <MenuList>Menu2</MenuList>
+          <MenuList>Menu3</MenuList>
+          <MenuList>Menu4</MenuList>
+        </ul>
 
-      <img
-        alt="Tripod logo"
-        src="http://via.placeholder.com/40x40"
-        className="nav__input"
-        onClick={() => (window.location.href = "/login")}
-      />
-
-      <img
-        alt="User logged"
-        src="http://via.placeholder.com/40x40"
-        className="nav__avatar"
-        onClick={() => (window.location.href = "/tripod")}
-      />
     </Container>
   );
 }
 
-const Container = styled.nav`
+const Container = styled.div`
   width: 100%;
   height: 4.6%;
 
-  z-index: 100;
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
 
   box-sizing: border-box;
 
-  background-color: skyblue;
+  background-color: #1d1f2c;
+
+  & > ul {
+    width: 68.3%;
+    height: 100%;
+
+    margin: 0;
+    padding: 0;
+
+    display: flex;
+
+    justify-content: space-between;
+    align-items: center;
+
+  }
+`;
+
+const MenuList = styled.li`
+  width: 18.3%;
+  height: 70%;
 
   display: flex;
 
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
+
+  list-style-type: none;
+
+  background-color: #14171f;
+
+  color: white;
+
+  font-size: 0.5rem;
+  font-weight: bold;
+
+  border-radius: 10px;
 `;
